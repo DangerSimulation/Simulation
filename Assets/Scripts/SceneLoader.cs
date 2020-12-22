e8157eed7c0f626c461296e1881cb1d9a11f22e7
JoschuaGoetz <joschua.goetz@gmail.com>
2020-12-22 13:54:50 +0100

    Add SceneLoader with loading animation

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public sealed class SceneLoader : MonoBehaviour
+{
+    private static readonly SceneLoader instance = new SceneLoader();
+
+    static SceneLoader() { }
+
+    private SceneLoader() { }
+
+    public static SceneLoader Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public UnityEvent OnLoadBegin = new UnityEvent();
+    public UnityEvent OnLoadEnd = new UnityEvent();
+    public ScreenFader screenFader = null;
+
+    private bool isLoading = false;
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded += SetActiveScene;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= SetActiveScene;
+    }
+
+    public void LoadNewScene(string sceneName)
+    {
+        if (!isLoading)
+            StartCoroutine(LoadScene(sceneName));
+    }
+
+    private IEnumerator LoadScene(string sceneName)
+    {
+        isLoading = true;
+
+        OnLoadBegin?.Invoke();
+        yield return screenFader.StartFadeIn();
+        yield return StartCoroutine(UnloadCurrent());
+
+        yield return new WaitForSeconds(3.0f);
+
+        yield return StartCoroutine(LoadNew(sceneName));
+        yield return screenFader.StartFadeOut();
+        OnLoadEnd?.Invoke();
+
+        isLoading = false;
+    }
+
+    private IEnumerator UnloadCurrent()
+    {
+        AsyncOperation unloadOperation = SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+
+        while(!unloadOperation.isDone)
+            yield return null;
+    }
+
+    private IEnumerator LoadNew(string sceneName)
+    {
+        AsyncOperation unloadOperation = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+
+        while (!unloadOperation.isDone)
+            yield return null;
+    }
+
+    private void SetActiveScene(Scene scene, LoadSceneMode mode)
+    {
+        SceneManager.SetActiveScene(scene);
+    }
+}
